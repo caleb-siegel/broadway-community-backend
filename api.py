1eca0cbe3271b255b573190bf5806e431b3d63b6
Caleb Siegel <caleb.siegel@gmail.com>
2024-09-27 08:35:01 -0400

    add new categories to endpoints

--- api.py
+++ api.py
@@ -34,31 +34,53 @@ show_api_endpoints = [
     {"name": "Taylor Swift", "link": "https://api.stubhub.net/catalog/categories/11113/events?exclude_parking_passes=true"},
 ]
 
-show_api_endpoints2 = [
-    {"name": "Hamilton", "category_id": "35042", "latitude": "40.759033203125", "longitude": "-73.98674774169922"},
-    {"name": "Six", "category_id": "82392", "latitude": "40.7599414", "longitude": "-73.98690909999999"},
-    {"name": "& Juliet", "category_id": "127739", "latitude": "40.75594711303711", "longitude": "-73.98497772216797"},
-    {"name": "MJ", "category_id": "177138", "latitude": "40.7631716", "longitude": "-73.9845438"},
-    {"name": "The Outsiders", "category_id": "102774", "latitude": "40.75878524780273", "longitude": "-73.98772430419922"},
-    {"name": "The Book of Mormon", "category_id": "150057581", "latitude": "40.7610848", "longitude": "-73.9857178"},
-    {"name": "Wicked", "category_id": "3682", "latitude": "40.759033203125", "longitude": "-73.98674774169922"},
-    {"name": "The Lion King", "category_id": "1534", "latitude": "40.759033203125", "longitude": "-73.98674774169922"},
-    {"name": "Hell's Kitchen", "category_id": "150087389", "latitude": "40.759033203125", "longitude": "-73.98674774169922"},
-    {"name": "Moulin Rouge", "category_id": "11874", "latitude": "40.759033203125", "longitude": "-73.98674774169922"},
-    {"name": "The Notebook", "category_id": "430684", "latitude": "40.759033203125", "longitude": "-73.98674774169922"},
-    {"name": "Harry Potter and the Cursed Child", "category_id": "36402", "latitude": "40.759033203125", "longitude": "-73.98674774169922"},
-    {"name": "Back to the Future", "category_id": "146928", "latitude": "40.759033203125", "longitude": "-73.98674774169922"},
-    {"name": "The Great Gatsby", "category_id": "42838", "latitude": "40.759033203125", "longitude": "-73.98674774169922"},
-    {"name": "Oh, Mary", "category_id": "150204986", "latitude": "40.759033203125", "longitude": "-73.98674774169922"},
-    {"name": "Aladdin", "category_id": "12721", "latitude": "40.759033203125", "longitude": "-73.98674774169922"},
-    {"name": "Chicago", "category_id": "14222", "latitude": "40.759033203125", "longitude": "-73.98674774169922"},
-    {"name": "Cabaret", "category_id": "4987", "latitude": "40.759033203125", "longitude": "-73.98674774169922"},
-    {"name": "Water for Elephants", "category_id": "150043364", "latitude": "40.759033203125", "longitude": "-73.98674774169922"},
-    {"name": "Elf", "category_id": "33322", "latitude": "40.759033203125", "longitude": "-73.98674774169922"},
-    {"name": "Suffs", "category_id": "429656", "latitude": "40.759033203125", "longitude": "-73.98674774169922"},
-    {"name": "Hadestown", "category_id": "101920", "latitude": "40.759033203125", "longitude": "-73.98674774169922"},
-    {"name": "Taylor Swift", "category_id": "11113", "latitude": "40.759033203125", "longitude": "-73.98674774169922"},
-]
+show_api_endpoints2 = {
+    "broadway": [
+        {"name": "Hamilton", "category_id": "35042", "latitude": "40.759033203125", "longitude": "-73.98674774169922"},
+        {"name": "Six", "category_id": "82392", "latitude": "40.7599414", "longitude": "-73.98690909999999"},
+        {"name": "& Juliet", "category_id": "127739", "latitude": "40.75594711303711", "longitude": "-73.98497772216797"},
+        {"name": "MJ", "category_id": "177138", "latitude": "40.7631716", "longitude": "-73.9845438"},
+        {"name": "The Outsiders", "category_id": "102774", "latitude": "40.75878524780273", "longitude": "-73.98772430419922"},
+        {"name": "The Book of Mormon", "category_id": "150057581", "latitude": "40.7610848", "longitude": "-73.9857178"},
+        {"name": "Wicked", "category_id": "3682", "latitude": "40.759033203125", "longitude": "-73.98674774169922"},
+        {"name": "The Lion King", "category_id": "1534", "latitude": "40.759033203125", "longitude": "-73.98674774169922"},
+        {"name": "Hell's Kitchen", "category_id": "150087389", "latitude": "40.759033203125", "longitude": "-73.98674774169922"},
+        {"name": "Moulin Rouge", "category_id": "11874", "latitude": "40.759033203125", "longitude": "-73.98674774169922"},
+        {"name": "The Notebook", "category_id": "430684", "latitude": "40.759033203125", "longitude": "-73.98674774169922"},
+        {"name": "Harry Potter and the Cursed Child", "category_id": "36402", "latitude": "40.759033203125", "longitude": "-73.98674774169922"},
+        {"name": "Back to the Future", "category_id": "146928", "latitude": "40.759033203125", "longitude": "-73.98674774169922"},
+        {"name": "The Great Gatsby", "category_id": "42838", "latitude": "40.759033203125", "longitude": "-73.98674774169922"},
+        {"name": "Oh, Mary", "category_id": "150204986", "latitude": "40.759033203125", "longitude": "-73.98674774169922"},
+        {"name": "Aladdin", "category_id": "12721", "latitude": "40.759033203125", "longitude": "-73.98674774169922"},
+        {"name": "Chicago", "category_id": "14222", "latitude": "40.759033203125", "longitude": "-73.98674774169922"},
+        {"name": "Cabaret", "category_id": "4987", "latitude": "40.759033203125", "longitude": "-73.98674774169922"},
+        {"name": "Water for Elephants", "category_id": "150043364", "latitude": "40.759033203125", "longitude": "-73.98674774169922"},
+        {"name": "Elf", "category_id": "33322", "latitude": "40.759033203125", "longitude": "-73.98674774169922"},
+        {"name": "Suffs", "category_id": "429656", "latitude": "40.759033203125", "longitude": "-73.98674774169922"},
+        {"name": "Hadestown", "category_id": "101920", "latitude": "40.759033203125", "longitude": "-73.98674774169922"},
+    ],
+    "taylor swift": [
+        {"name": "Taylor Swift", "category_id": "11113", "latitude": "", "longitude": ""},
+    ],
+    "yankees playoffs": [
+        {"name": "All Yankees", "category_id": "6017", "latitude": "", "longitude": ""},
+        {"name": "Yankees at Home", "category_id": "6017", "latitude": "40.8352765", "longitude": "-73.921855"},
+        {"name": "Yankees Spring Training", "category_id": "150366110", "latitude": "27.9792251", "longitude": "-82.5078734", "max_distance": 40000},
+        {"name": "AL Wild Card", "category_id": "150366110", "latitude": "", "longitude": ""},
+        {"name": "ALDS", "category_id": "263022", "latitude": "", "longitude": ""},
+        {"name": "ALCS", "category_id": "262850", "latitude": "", "longitude": ""},
+        {"name": "World Series", "category_id": "6601", "latitude": "", "longitude": ""},
+        {"name": "Yankees Wild Card", "category_id": "150366110", "latitude": "40.8352765", "longitude": "-73.921855"},
+        {"name": "Yankees ALDS", "category_id": "263022", "latitude": "40.8352765", "longitude": "-73.921855"},
+        {"name": "Yankees ALCS", "category_id": "262850", "latitude": "40.8352765", "longitude": "-73.921855"},
+        {"name": "Yankees World Series", "category_id": "6601", "latitude": "40.8352765", "longitude": "-73.921855"},
+    ],
+    "world cup 2026": [
+        {"name": "All World Cup", "category_id": "278322", "latitude": "", "longitude": ""},
+        {"name": "World Cup Metlife", "category_id": "278322", "latitude": "40.8241653442383", "longitude": "-74.0868377685547"},
+    ],
+}
+
 
 def get_link(category_id, latitude=None, longitude=None, max_distance=1000):
     # add starting point
