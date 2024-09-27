465851422ede97b2b022632c91e735f572e6c81a
Caleb Siegel <caleb.siegel@gmail.com>
2024-09-27 08:36:55 -0400

    remove old endpoints variable

--- api.py
+++ api.py
@@ -8,33 +8,7 @@ token_cache = {
     "expires_at": None
 }
 
-show_api_endpoints = [
-    {"name": "Hamilton", "link": "https://api.stubhub.net/catalog/categories/35042/events?exclude_parking_passes=true&latitude=40.759033203125&longitude=-73.98674774169922&max_distance_in_meters=1000"},
-    {"name": "Six", "link": "https://api.stubhub.net/catalog/categories/82392/events?exclude_parking_passes=true&latitude=40.7599414&longitude=-73.98690909999999&max_distance_in_meters=1000"},
-    {"name": "& Juliet", "link": "https://api.stubhub.net/catalog/categories/127739/events?exclude_parking_passes=true&latitude=40.75594711303711&longitude=-73.98497772216797&max_distance_in_meters=1000"},
-    {"name": "MJ", "link": "https://api.stubhub.net/catalog/categories/177138/events?exclude_parking_passes=true&latitude=40.7631716&longitude=-73.9845438&max_distance_in_meters=1000"},
-    {"name": "The Outsiders", "link": "https://api.stubhub.net/catalog/categories/102774/events?exclude_parking_passes=true&latitude=40.75878524780273&longitude=-73.98772430419922&max_distance_in_meters=1000"},
-    {"name": "The Book of Mormon", "link": "https://api.stubhub.net/catalog/categories/150057581/events?exclude_parking_passes=true&latitude=40.7610848&longitude=-73.9857178&max_distance_in_meters=1000"},
-    {"name": "Wicked", "link": "https://api.stubhub.net/catalog/categories/3682/events?exclude_parking_passes=true&latitude=40.7610848&longitude=-73.9857178&max_distance_in_meters=1000"},
-    {"name": "The Lion King", "link": "https://api.stubhub.net/catalog/categories/1534/events?exclude_parking_passes=true&latitude=40.7610848&longitude=-73.9857178&max_distance_in_meters=1000"},
-    {"name": "Hell's Kitchen", "link": "https://api.stubhub.net/catalog/categories/150087389/events?exclude_parking_passes=true&latitude=40.7610848&longitude=-73.9857178&max_distance_in_meters=1000"},
-    {"name": "Moulin Rouge", "link": "https://api.stubhub.net/catalog/categories/11874/events?exclude_parking_passes=true&latitude=40.7610848&longitude=-73.9857178&max_distance_in_meters=1000"},
-    {"name": "The Notebook", "link": "https://api.stubhub.net/catalog/categories/430684/events?exclude_parking_passes=true&latitude=40.7610848&longitude=-73.9857178&max_distance_in_meters=1000"},
-    {"name": "Harry Potter and the Cursed Child", "link": "https://api.stubhub.net/catalog/categories/36402/events?exclude_parking_passes=true&latitude=40.7610848&longitude=-73.9857178&max_distance_in_meters=1000"},
-    {"name": "Back to the Future", "link": "https://api.stubhub.net/catalog/categories/146928/events?exclude_parking_passes=true&latitude=40.7610848&longitude=-73.9857178&max_distance_in_meters=1000"},
-    {"name": "The Great Gatsby", "link": "https://api.stubhub.net/catalog/categories/42838/events?exclude_parking_passes=true&latitude=40.7610848&longitude=-73.9857178&max_distance_in_meters=1000"},
-    {"name": "Oh, Mary", "link": "https://api.stubhub.net/catalog/categories/150204986/events?exclude_parking_passes=true&latitude=40.7610848&longitude=-73.9857178&max_distance_in_meters=1000"},
-    {"name": "Aladdin", "link": "https://api.stubhub.net/catalog/categories/12721/events?exclude_parking_passes=true&latitude=40.7610848&longitude=-73.9857178&max_distance_in_meters=1000"},
-    {"name": "Chicago", "link": "https://api.stubhub.net/catalog/categories/14222/events?exclude_parking_passes=true&latitude=40.7610848&longitude=-73.9857178&max_distance_in_meters=1000"},
-    {"name": "Cabaret", "link": "https://api.stubhub.net/catalog/categories/4987/events?exclude_parking_passes=true&latitude=40.7610848&longitude=-73.9857178&max_distance_in_meters=1000"},
-    {"name": "Water for Elephants", "link": "https://api.stubhub.net/catalog/categories/150043364/events?exclude_parking_passes=true&latitude=40.7610848&longitude=-73.9857178&max_distance_in_meters=1000"},
-    {"name": "Elf", "link": "https://api.stubhub.net/catalog/categories/33322/events?exclude_parking_passes=true&latitude=40.7610848&longitude=-73.9857178&max_distance_in_meters=1000"},
-    {"name": "Suffs", "link": "https://api.stubhub.net/catalog/categories/429656/events?exclude_parking_passes=true&latitude=40.7610848&longitude=-73.9857178&max_distance_in_meters=1000"},
-    {"name": "Hadestown", "link": "https://api.stubhub.net/catalog/categories/101920/events?exclude_parking_passes=true&latitude=40.7610848&longitude=-73.9857178&max_distance_in_meters=1000"},
-    {"name": "Taylor Swift", "link": "https://api.stubhub.net/catalog/categories/11113/events?exclude_parking_passes=true"},
-]
-
-show_api_endpoints2 = {
+show_api_endpoints = {
     "broadway": [
         {"name": "Hamilton", "category_id": "35042", "latitude": "40.759033203125", "longitude": "-73.98674774169922"},
         {"name": "Six", "category_id": "82392", "latitude": "40.7599414", "longitude": "-73.98690909999999"},
